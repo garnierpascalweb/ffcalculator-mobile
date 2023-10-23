81ad5b267bad33aa9f7cd3b509f71bfc81dac372
pierre <garnierpascalweb@gmail.com>
2023-10-23 23:51:14 +0200

    1.0.0: appropriation Application et reference dans le manifest

---
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/FFCalculatorApplication.java
@@ -0,0 +1,27 @@
+package com.gpwsofts.ffcalculator.mobile;
+
+import android.app.Application;
+
+public class FFCalculatorApplication extends Application {
+
+    public static FFCalculatorApplication instance;
+
+    public FFCalculatorApplication() {
+
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+}
--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/notifications/NotificationsFragment.java
@@ -10,7 +10,19 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.core.ui.Legend;
+import com.anychart.enums.LegendLayout;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pyramid;
+import com.anychart.enums.Align;
+import com.gpwsofts.ffcalculator.mobile.R;
 import com.gpwsofts.ffcalculator.mobile.databinding.FragmentNotificationsBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
@@ -23,6 +35,23 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        //PG chart
+        AnyChartView anyChartView = binding.anyChartView;
+        Pyramid pyramid = AnyChart.pyramid();
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("TV promotion", 6371664));
+        data.add(new ValueDataEntry("Radio promotion", 7216301));
+        data.add(new ValueDataEntry("Advertising leaflets", 1486621));
+        data.add(new ValueDataEntry("Before advertising started", 1386622));
+        pyramid.data(data);
+        Legend legend = pyramid.legend();
+        legend.enabled(true);
+        legend.position("outside-right");
+        legend.itemsLayout(LegendLayout.VERTICAL);
+        legend.align(Align.TOP);
+        pyramid.labels(false);
+        anyChartView.setChart(pyramid);
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
