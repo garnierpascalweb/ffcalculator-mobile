b875052e3bb423e112216d47987135cbb7d99505
Garnier <pascalscn@hotmail.com>
2023-11-16 11:26:10 +0100

    1.0.0: addresult : mise en place de lecran de saisie de nouveau resultat

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.gpwsofts.ffcalculator.mobile.dao.Result;
 import com.gpwsofts.ffcalculator.mobile.databinding.FragmentHomeBinding;
 import com.gpwsofts.ffcalculator.mobile.viewmodel.ResultViewModel;
@@ -31,10 +34,14 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextInputEditText textInputEditTextPlace = binding.idTIETPlace;
+        final TextInputLayout textInputLayoutSpinnerClasses = binding.idTISPClasses;
+        final TextInputLayout textInputLayoutSpinnerPos = binding.idTISPPos;
+        final TextInputLayout textInputLayoutSpinnerPrts = binding.idTISPPrts;
+        final Button buttonAjouter = binding.idBTAjouter;
 
         // selon https://stackoverflow.com/questions/63548323/how-to-use-viewmodel-in-a-fragment
+        /*
         resultViewModel = new ViewModelProvider(requireActivity()).get(ResultViewModel.class);
         resultViewModel.getAllResults().observe(getActivity(), new Observer<List<Result>>(){
                     @Override
@@ -44,6 +51,7 @@ public class HomeFragment extends Fragment {
                     }
                 }
                 );
+         */
         return root;
     }
 
