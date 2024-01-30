15ec184361d4c4a432ce21488352c2fd216c628a
Garnier <pascalscn@hotmail.com>
2024-01-30 20:07:40 +0100

    1.0.0: fragment result: ajouter resultat: branchement ihm

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/result/ResultFragment.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/result/ResultFragment.java
@@ -137,21 +137,30 @@ public class ResultFragment extends Fragment {
         return root;
     }
 
+    /**
+     * Demande de sauvegarde d'un resultat
+     * @since 1.0.0
+     */
     private void saveResult() {
-        String place = String.valueOf(this.textInputEditTextPlace.getText());
-        //String classe = String.valueOf(this.textInputLayoutSpinnerClasses.getEditText().getText());
-        String classe = "1.25.1";
+        // recuperation des datas
+        final String place = String.valueOf(textInputEditTextPlace.getText());
+        final String libelle = String.valueOf(autoCompleteTextViewClasses.getText().toString());
+        final int pos = Integer.valueOf(autoCompleteTextViewPos.getText().toString());
+        final int prts = Integer.valueOf(autoCompleteTextViewPrts.getText().toString());
         Log.i(TAG_NAME, "ajout de la course " + place);
         Result result = new Result();
-        result.setPlace("Trigance");
+        result.setPlace(place);
+        //TODO 1.0.0 : creer un logo service pour rendr eun logo en fonction de la classe de course
         result.setLogo("Open 1/2/3");
         result.setPts(10.29);
-        result.setPrts(147);
-        result.setPos(16);
-        result.setLibelle("Open 1/2/3 Access");
+        result.setPrts(prts);
+        result.setPos(pos);
+        result.setLibelle(libelle);
+        // TODO 1.0.0 ecrire un service qui extrait le idclasse depuis le libelle
         result.setIdClasse("1.24.0");
         resultViewModel.insert(result);
         Toast.makeText(this.getContext(), "Result Save", Toast.LENGTH_SHORT).show();
+        //TODO 1.0.0 gerer les erreurs
     }
 
     @Override
