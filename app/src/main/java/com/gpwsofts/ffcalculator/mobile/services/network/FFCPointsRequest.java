fb0f5843428f9f55bb30617e24d9875c1efbc883
pierre <garnierpascalweb@gmail.com>
2024-02-09 10:59:49 +0100

    1.0.0: appels reseaux: retrofit: mise en place de interface, pojo, etc

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/network/FFCPointsRequest.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/network/FFCPointsRequest.java
@@ -2,6 +2,10 @@ package com.gpwsofts.ffcalculator.mobile.services.network;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * @since 1.0.0
+ * POJO pour une requete de calcul de points
+ */
 public class FFCPointsRequest {
 
     @SerializedName("prts")public Integer prts;
--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/network/IApiInterface.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/network/IApiInterface.java
@@ -5,6 +5,7 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 /**
+ * @since 1.0.0
  * Interface de la couche cliente API
  */
 public interface IApiInterface {
