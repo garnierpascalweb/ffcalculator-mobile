27ace5cce3145c91ed6419e1a59d849c62b5a905
pierre <garnierpascalweb@gmail.com>
2024-02-15 19:17:39 +0100

    1.0.0: Synthesis: calcul dynamique du nombre total de points et de la position, le passage en Shared Preferences est pas utile

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/synthesis/SynthesisFragment.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/synthesis/SynthesisFragment.java
@@ -9,60 +9,70 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.gpwsofts.ffcalculator.mobile.FFCalculatorApplication;
 import com.gpwsofts.ffcalculator.mobile.constants.SharedPreferencesConstants;
 import com.gpwsofts.ffcalculator.mobile.databinding.FragmentSynthesisBinding;
+import com.gpwsofts.ffcalculator.mobile.services.network.FFCPosResponse;
 import com.gpwsofts.ffcalculator.mobile.ui.result.ResultViewModel;
 import com.gpwsofts.ffcalculator.mobile.ui.shared.SharedPrefsViewModel;
+
+import java.util.Comparator;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SynthesisFragment extends Fragment {
     private static final String TAG_NAME = "SynthesisFragment";
     private FragmentSynthesisBinding binding;
 
+    private ResultViewModel resultViewModel;
+    private SharedPrefsViewModel sharedPrefsViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        resultViewModel = new ViewModelProvider(this).get(ResultViewModel.class);
+        sharedPrefsViewModel = new ViewModelProvider(this).get(SharedPrefsViewModel.class);
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         SynthesisViewModel notificationsViewModel = new ViewModelProvider(this).get(SynthesisViewModel.class);
-        SharedPrefsViewModel sharedPrefsViewModel = new ViewModelProvider(getActivity()).get(SharedPrefsViewModel.class);
-        ResultViewModel resultViewModel = new ViewModelProvider(getActivity()).get(ResultViewModel.class);
         binding = FragmentSynthesisBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        /*
-        //PG chart
-        final String[] colors = new String[] {"#F5B7B1", "#3498DB", "#85C1E9", "#AED6F1"};
-        AnyChartView anyChartView = binding.anyChartView;
-        Pyramid pyramid = AnyChart.pyramid();
-        List<DataEntry> data = new ArrayList<>();
-
-        // On part de la base dez la pyramide
-        data.add(new ValueDataEntry(getString(R.string.label_open3), 45));
-        data.add(new ValueDataEntry(getString(R.string.label_open2), 30));
-        data.add(new ValueDataEntry(getString(R.string.label_open1), 20));
-        data.add(new ValueDataEntry(getString(R.string.label_elite), 5));
-
-
-
-        pyramid.data(data);
-
-        Legend legend = pyramid.legend();
-        legend.enabled(true);
-        legend.position("outside-right");
-        legend.itemsLayout(LegendLayout.VERTICAL);
-        legend.align(Align.TOP);
-
-        pyramid.labels(false);
-        //pyramid.fill(colors);
-        anyChartView.setChart(pyramid);
-         */
-
-
         final TextView textView = binding.textNotifications;
         final TextView textViewPts = binding.textAllpts;
         final TextView textViewPos = binding.textMypos;
-        // notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextInputEditText textInput = binding.idTIEVue;
+        resultViewModel.getAllResults().observe(getViewLifecycleOwner() , results -> {
+            final double allPts = results.stream().mapToDouble(result -> result.getPts()).sorted().limit(15).sum();
+            textViewPts.setText("Nombre de POINTS = " + allPts);
+            final Call<FFCPosResponse> call = FFCalculatorApplication.instance.getServicesManager().getPosService().calcPos(allPts, "H");
+            call.enqueue(new Callback<FFCPosResponse>() {
+                @Override
+                public void onResponse(Call<FFCPosResponse> call, Response<FFCPosResponse> response) {
+                    if (response.isSuccessful()) {
+                        int myPos = response.body().pos;
+                        //sharedPrefsViewModel.updatePos(myPos);
+                        textViewPos.setText(myPos + " eme au classement national FFC ");
+                        Log.d(TAG_NAME,"recalcul de la position ok : <" + myPos + ">");
+                    } else {
+                        Log.w(TAG_NAME, "probleme sur le calcul de la position " + response.code());
+                    }
+                }
+                @Override
+                public void onFailure(Call<FFCPosResponse> call, Throwable t) {
+                    Log.w(TAG_NAME, "probleme sur l'appel au calcul de la position");
+                }
+            });
+        });
         sharedPrefsViewModel.getVue().getStringLiveData(SharedPreferencesConstants.KEY_VUE, SharedPreferencesConstants.DEFAULT_VALUE_VUE).observe(getViewLifecycleOwner(), new Observer<String>(){
             @Override
             public void onChanged(String s) {
@@ -88,18 +98,6 @@ public class SynthesisFragment extends Fragment {
             }
         });
 
-
-        final TextInputEditText textInput = binding.idTIEVue;
-        final Button button = binding.idBTVue;
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Update la vue
-                String newVue = textInput.getText().toString();
-                Log.i(TAG_NAME, "mise a jour de la vue vers " + newVue);
-                sharedPrefsViewModel.updateVue(newVue);
-            };
-        });
         return root;
     }
 
