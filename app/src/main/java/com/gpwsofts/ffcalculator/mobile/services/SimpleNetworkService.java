430be7fbe3afa6ae4189ea309ff5a547ad481539
Garnier <pascalscn@hotmail.com>
2024-02-28 00:25:44 +0100

    1.0.0: assets: mise a jour de grilles.csv: nouveau champ priority et revie des vues (G) et libelles

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/SimpleNetworkService.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/SimpleNetworkService.java
@@ -5,6 +5,7 @@ import com.gpwsofts.ffcalculator.mobile.services.network.INetworkService;
 public class SimpleNetworkService implements INetworkService {
     @Override
     public boolean isWwwConnected() {
+        //TODO 1.0.0 dire la verité concernant ca
         return false;
     }
 }
