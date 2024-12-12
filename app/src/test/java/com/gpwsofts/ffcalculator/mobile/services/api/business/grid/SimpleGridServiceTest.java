fe6fa7980f9e95c8b41c3c4ecbed712b2d15c277
Garnier <pascalscn@hotmail.com>
2024-12-12 09:47:34 +0100

    1.0.0: tests unitaires: ajout de testImplementation 'org.mockito:mockito-core:1.10.19

---
+++ app/src/test/java/com/gpwsofts/ffcalculator/mobile/services/api/business/grid/SimpleGridServiceTest.java
@@ -0,0 +1,21 @@
+package com.gpwsofts.ffcalculator.mobile.services.api.business.grid;
+
+import static org.junit.Assert.assertNotNull;
+
+import com.gpwsofts.ffcalculator.mobile.model.Grid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleGridServiceTest {
+    @Test
+    public void getGrids() {
+        SimpleGridService sgs = new SimpleGridService();
+        List<Grid> grids = sgs.getGrids();
+        assertNotNull("la liste des grilles ne devrait pas etre nulle", grids);
+        System.out.println(grids.size()  + " grilles chargées");
+    }
+}
