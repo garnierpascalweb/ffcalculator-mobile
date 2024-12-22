a29fdf17a304175da0d77b506044c897e3e69508
Garnier <pascalscn@hotmail.com>
2024-12-22 15:16:34 +0100

    1.0.0: design: labels de classement et de pts dans la vue saison

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/pts/SimplePtsHttpService.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/pts/SimplePtsHttpService.java
@@ -23,9 +23,11 @@ public class SimplePtsHttpService  implements IPtsHttpService {
     @Override
     public Call<FFCPointsResponse> calcPts(String place, int pos, int prts, String classe) {
         // cest du retrofit !
-        Log.i(TAG_NAME, "demand ede calcul des points pour une place de <" + pos + "> sur <" + prts + "> sur la course de <" + place + "> en serie <" + classe + ">");
+        Log.i(TAG_NAME, "demande de calcul des points pour sur la course de <" + place + "> pour une place de <" + pos + "> sur <" + prts + "> sur la course de <" + place + "> en serie <" + classe + ">");
         Log.d(TAG_NAME, "construction de la requete");
         FFCPointsRequest request = new FFCPointsRequest();
+        //TODO 1.0.0 se premunir d'injections sql
+        request.place = place;
         request.code = classe;
         request.pos = pos;
         request.prts = prts;
