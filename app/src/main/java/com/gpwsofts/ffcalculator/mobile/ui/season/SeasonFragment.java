311e7879ff11dc64f5a5c7c3d2050ed9e2dfbda4
Garnier <pascalscn@hotmail.com>
2025-01-22 21:08:03 +0100

    1.0.0: frangment Season: mise en place du total des points et du classement national

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/season/SeasonFragment.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/ui/season/SeasonFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,19 +15,28 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.anychart.chart.common.dataentry.SingleValueDataSet;
 import com.gpwsofts.ffcalculator.mobile.R;
 import com.gpwsofts.ffcalculator.mobile.databinding.FragmentSeasonBinding;
+import com.gpwsofts.ffcalculator.mobile.ui.synthesis.SynthesisViewModel;
+import com.gpwsofts.ffcalculator.mobile.ui.view.VueViewModel;
 
 public class SeasonFragment extends Fragment {
 
     private static final String TAG_NAME = "SeasonFragment";
     private FragmentSeasonBinding binding;
     private SeasonViewModel resultViewModel;
+    private SynthesisViewModel synthesisViewModel;
+    private VueViewModel vueViewModel;
+    private TextView textViewPts;
+    private TextView textViewPos;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         resultViewModel = new ViewModelProvider(this).get(SeasonViewModel.class);
+        synthesisViewModel = new ViewModelProvider(this).get(SynthesisViewModel.class);
+        vueViewModel = new ViewModelProvider(this).get(VueViewModel.class);
     }
 
     @Override
@@ -35,6 +45,8 @@ public class SeasonFragment extends Fragment {
         binding = FragmentSeasonBinding.inflate(inflater, container, false);
         // binding donne la vue
         View root = binding.getRoot();
+        textViewPts = binding.textAllpts;
+        textViewPos = binding.textMypos;
         // RecyclerView
         RecyclerView resultRV = root.findViewById(R.id.idRVCourse);
         final ResultListAdapter adapter = new ResultListAdapter(new ResultListAdapter.ResultDiff());
@@ -63,6 +75,37 @@ public class SeasonFragment extends Fragment {
             Log.i(TAG_NAME, "fin observer getAllResults");
         });
 
+        // observation du total des points
+        // Update UI
+        synthesisViewModel.getPts().observe(getViewLifecycleOwner(), pts -> {
+            Log.i(TAG_NAME, "debut observer getPts");
+            if (null != pts) {
+                textViewPts.setText(getString(R.string.label_total_pts, pts));
+                final String classType = vueViewModel.getVueLiveData().getValue().getMapClass();
+                searchPosApi(pts, classType);
+            } else {
+                textViewPts.setText(getString(R.string.label_aucun_resultat));
+                textViewPos.setText("");
+                Log.w(TAG_NAME, "la valeur de pts est pas renseignee");
+            }
+            Log.i(TAG_NAME, "fin observer getPts");
+        });
+
+        // observation du total des positions
+        // Update UI
+        synthesisViewModel.getPos().observe(getViewLifecycleOwner(), pos -> {
+            Log.i(TAG_NAME, "debut observer getPos");
+            final String classType = vueViewModel.getVueLiveData().getValue().getMapClass();
+            if (pos != null) {
+                // textViewPos.setText("Classement National : " + pos + " eme");
+                textViewPos.setText(getString(R.string.label_classement_national, classType, pos));
+            } else {
+                textViewPos.setText(getString(R.string.label_classement_national_ko));
+                Log.w(TAG_NAME, "la valeur de pos est pas renseignee apres appel au service");
+            }
+            Log.i(TAG_NAME, "fin observer getPos");
+        });
+
         //TODO 1.0.0 le swip ne marche pas dans le fragment puisque le swip est porté par le fragment
 
 
@@ -75,5 +118,8 @@ public class SeasonFragment extends Fragment {
         binding = null;
     }
 
+    private void searchPosApi(double pts, String classType) {
+        synthesisViewModel.searchPosApi(pts, classType);
+    }
 
 }
