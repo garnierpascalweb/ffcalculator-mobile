83f0b4117ccf83c5d82536968553d4e9980e6f3b
Garnier <pascalscn@hotmail.com>
2025-02-25 21:20:46 +0100

    1.0.0: miqse en place de saveurs dev prod

---
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/utils/LogUtils.java
@@ -0,0 +1,33 @@
+package com.gpwsofts.ffcalculator.mobile.utils;
+
+import android.util.Log;
+
+import com.gpwsofts.ffcalculator.mobile.BuildConfig;
+
+public class LogUtils {
+    private static final boolean ENABLE_LOGS = BuildConfig.ENABLE_LOGS;
+
+    public static void d(String tag, String message) {
+        if (ENABLE_LOGS) {
+            Log.d(tag, message);
+        }
+    }
+
+    public static void e(String tag, String message) {
+        if (ENABLE_LOGS) {
+            Log.e(tag, message);
+        }
+    }
+
+    public static void i(String tag, String message) {
+        if (ENABLE_LOGS) {
+            Log.i(tag, message);
+        }
+    }
+
+    public static void w(String tag, String message) {
+        if (ENABLE_LOGS) {
+            Log.w(tag, message);
+        }
+    }
+}
