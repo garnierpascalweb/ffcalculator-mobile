e2796a7d3fb767b71f0806f06e13761e783c1951
Garnier <pascalscn@hotmail.com>
2025-03-29 18:16:48 +0100

    1.0.0: renommage de l'application en FFCalculator

--- app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/client/AddResultApiClient.java
+++ app/src/main/java/com/gpwsofts/ffcalculator/mobile/services/client/AddResultApiClient.java
@@ -126,12 +126,6 @@ public class AddResultApiClient extends AbstractApiClient {
                             newResult = createResultFromDatas(request.code, request.place, libelle, request.pos, request.prts, pts);
                             message = FFCalculatorApplication.instance.getResources().getString(R.string.toast_add_result_ok);
                             isOk = true;
-                            // TODO 1.0.0 simple testicule
-                            AddResultException ade = new AddResultException("message fake exception");
-                            ade.setToastMessage("toast message");
-                            //FFCalculatorWebApi.getInstance().getApiService().sendReport(FFCalculatorSharedPrefs.id(), BuildConfig.FLAVOR, FFCReportRequestFactory.createFFCReportRequest(TAG_NAME,ade));
-                            sendErrorToBackEnd(TAG_NAME, ade);
-                            FFCalculatorApplication.instance.getServicesManager().getReportService().sendReport(FFCReportRequestFactory.createFFCReportRequest(TAG_NAME,ade));
                         } else {
                             AddResultException ade = new AddResultException("probleme sur le calcul des points - reponse serveur non exploitable - null");
                             ade.setToastMessage(FFCalculatorApplication.instance.getResources().getString(R.string.toast_technical_problem));
